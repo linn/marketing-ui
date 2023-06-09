a54f4d69d34f10caf8d2e0f64168157e8bf4e5a7
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2023-06-09 16:26:28 +0100

    try routes first

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
-
 app.get('/marketing-ui', function (_, res) {
+  console.log(__dirname);
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.get('/healthcheck', (_, res) => res.sendStatus(200));
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(5050);
