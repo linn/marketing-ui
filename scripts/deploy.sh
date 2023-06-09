d2808fa6d7d181ed98b03e2715df90da19aa975a
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2023-06-09 15:14:11 +0100

    fix script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,6 +17,8 @@ if [ "${TRAVIS_BRANCH}" = "main" ]; then
     APP_ROOT=http://app.linn.co.uk
     PROXY_ROOT=http://app.linn.co.uk
   	ENV_SUFFIX=
+    aws cloudformation deploy --stack-name $STACK_NAME --template-file ./aws/application.yml --parameter-overrides dockerTag=$TRAVIS_BUILD_NUMBER appRoot=$APP_ROOT environmentSuffix=$ENV_SUFFIX --capabilities=CAPABILITY_IAM
+
   else
     # pull request based on master - deploy to sys
     echo deploy to sys
@@ -27,6 +29,8 @@ if [ "${TRAVIS_BRANCH}" = "main" ]; then
     APP_ROOT=http://app-sys.linn.co.uk
     PROXY_ROOT=http://app.linn.co.uk
     ENV_SUFFIX=-sys
+    aws cloudformation deploy --stack-name $STACK_NAME --template-file ./aws/application.yml --parameter-overrides dockerTag=$TRAVIS_BUILD_NUMBER appRoot=$APP_ROOT environmentSuffix=$ENV_SUFFIX --capabilities=CAPABILITY_IAM
+
   fi
 else
   # not main - deploy to int if required
@@ -37,6 +41,5 @@ fi
 # source ./secrets.env > /dev/null 2>&1
 
 # deploy the service to amazon
-aws cloudformation deploy --stack-name $STACK_NAME --template-file ./aws/application.yml --parameter-overrides dockerTag=$TRAVIS_BUILD_NUMBER appRoot=$APP_ROOT environmentSuffix=$ENV_SUFFIX --capabilities=CAPABILITY_IAM
 
 echo "deploy complete"
