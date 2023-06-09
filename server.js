6f39972ed004bbf0693a17487487b497faf97da2
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2023-06-09 17:32:23 +0100

    try fix static files

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+app.use('/marketing-ui', express.static(__dirname + '/marketing-ui'))
 app.get('/marketing-ui', function (_, res) {
   console.log(__dirname);
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'marketing-ui', 'index.html'));
 });
 
 app.get('/healthcheck', (_, res) => res.sendStatus(200));
 
-app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.listen(5050);
