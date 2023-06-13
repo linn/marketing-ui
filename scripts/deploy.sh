f5775934c3580f786405a378379560dbb16044d7
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2023-06-13 10:49:17 +0100

    try fix docker build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,9 @@ docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
 if [ "${TRAVIS_BRANCH}" = "main" ]; then
   if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    docker build --no-cache -t linn/marketing-ui:$TRAVIS_BUILD_NUMBER --build-arg gitBranch=$GIT_BRANCH publicUrl=https://app.linn.co.uk/marketing-ui .
+    docker build --no-cache -t linn/marketing-ui:$TRAVIS_BUILD_NUMBER --build-arg gitBranch=$GIT_BRANCH --build-arg publicUrl=https://app.linn.co.uk/marketing-ui .
   else
-   docker build --no-cache -t linn/marketing-ui:$TRAVIS_BUILD_NUMBER --build-arg gitBranch=$GIT_BRANCH publicUrl=https://app-sys.linn.co.uk/marketing-ui .
+   docker build --no-cache -t linn/marketing-ui:$TRAVIS_BUILD_NUMBER --build-arg gitBranch=$GIT_BRANCH --build-arg publicUrl=https://app-sys.linn.co.uk/marketing-ui .
   fi
 fi
 
